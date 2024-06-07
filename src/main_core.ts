0da3d8c50c24a1f0082071d187733c254418f2d4
codeRongRong <13990259+coderongrong@user.noreply.gitee.com>
2024-06-07 17:45:42 +0800

    update

---
+++ src/main_core.ts
@@ -0,0 +1,12 @@
+/* 联合类型 */
+let numOrString: number | string;
+console.log('numOrString', numOrString);
+
+
+let a: string | number | undefined
+console.log('a?.toString()', a?.toString());
+
+
+
+
+export {}
